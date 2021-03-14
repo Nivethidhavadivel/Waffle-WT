99229070ec605ac521083561db8b0b0c8469eb98
Kevin Paul <kevinpaul1100@gmail.com>
2021-03-14 18:01:20 +0530

    Spring init

--- Server/waffle/src/main/java/com/assn/waffle/WaffleApplication.java
+++ Server/waffle/src/main/java/com/assn/waffle/WaffleApplication.java
@@ -1,4 +1,4 @@
-package com.waffle.waffle;
+package com.assn.waffle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Server/waffle/src/test/java/com/assn/waffle/WaffleApplicationTests.java
+++ Server/waffle/src/test/java/com/assn/waffle/WaffleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.waffle.waffle;
+package com.assn.waffle;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
