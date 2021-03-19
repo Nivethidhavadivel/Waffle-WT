c6935802c98878ea1a67d7b958da586394ed08e5
Kevin Paul <kevinpaul1100@gmail.com>
2021-03-19 17:56:17 +0530

    Mongo mvn

---
+++ Server/src/main/java/com/assnpresn/wafflebackend/WaffleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assnpresn.wafflebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaffleBackendApplication.class, args);
+    }
+
+}
