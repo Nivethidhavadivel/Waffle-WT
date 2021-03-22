405114a7ab63c65a04dc49e725cc9c8f29f83582
Kevin Paul <kevinpaul1100@gmail.com>
2021-03-22 19:56:53 +0530

    CSS

--- Client/waffle-fe/src/App.js
+++ Client/waffle-fe/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from "./Components/Routes";
+import {Navbar} from 'react-bootstrap'
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1> WAFFLE </h1>
+        <Navbar bg="light" expand="lg">
+          <h3 style={{color: "red"}}> WAFFLE </h3>
+        </Navbar>
+        
         <Routes/>
       </div>
     </Router>
